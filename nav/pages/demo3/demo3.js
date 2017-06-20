92c3b95c1a745d5835fcb80c8a16fbc84c07e754
小树 <menm91@sina.cn>
2017-06-20 13:26:31 +0800

    导航api

--- nav/pages/demo3/demo3.js
+++ nav/pages/demo3/demo3.js
@@ -1,66 +1,12 @@
 // pages/demo3/demo3.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  backHandler: function(){
+    //默认返回上一页
+    // wx.navigateBack();
+    // 指定delta，后退的步数
+    // delta 指定过大,返回最开始的页面
+    wx.navigateBack({
+      delta: 100
+    });
   }
 })
